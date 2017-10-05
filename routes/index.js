c00ce7b51851368e429ebc86a36c2b7c55af1983
pawbibo <jesus.cabelin@bibo.com.ph>
2017-10-05 11:03:54 +0800

    add new route for db query

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const tablesController = require('../controllers/tablesController');
+const db = require('../db');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'lazyquery', tables: db.tables });
 });
+
+router.get('/tables/:table', tablesController.showTable);
+router.get('/table/query', tablesController.query);
 
 module.exports = router;
